8c2cf1e824d2b949937f0cde432d9119367df150
Eric Ianni <iannie@onid.oregonstate.edu>
2015-02-11 18:32:43 -0500

    Tasks: Have add and delete working. Trying to get UPDATE to work.

--- videoStore.js
+++ videoStore.js
@@ -20,20 +20,70 @@ function request (action) {
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 			localStorage.setItem('videos', xmlhttp.responseText);
+			display();
 		}
 	}
-	display();
 }
 
 function display() {
 	var videos = JSON.parse(localStorage.getItem('videos'));
 	var container = document.getElementById('videos');
-	var elem;
+	var video;
+	var deleteButton;
 	for (var prop in videos) {
-		elem = document.createElement('div');
-		elem.setAttribute('id', videos[prop]['id']);
-		elem.innerHTML = videos[prop]['name'];
-		container.appendChild(elem);
+		video = document.createElement('div');
+		video.setAttribute('id', videos[prop]['id']);
+		video.setAttribute('class', 'video');
+		video.innerHTML = videos[prop]['name'];
+		deleteButton = document.createElement('div')
+		deleteButton.setAttribute('class', 'delete');
+		deleteButton.setAttribute('onclick', 'deleteFunction(this.parentNode)');
+		deleteButton.innerHTML = 'Delete';
+		rentButton = document.createElement('div')
+		rentButton.setAttribute('class', 'rent');
+		rentButton.setAttribute('onclick', 'flipRent(this.parentNode)');
+		var rented = JSON.parse(localStorage.getItem('rented'));
+		if(rented) {
+			rentButton.innerHTML = 'Return';	
+		} else {
+			rentButton.innerHTML = 'Rent';
+		}
+		
+		video.appendChild(deleteButton);
+		video.appendChild(rentButton);
+		container.appendChild(video);
 	}
 }
 
+function add(form) {
+	var name = form.name.value;
+	var category = form.category.value;
+	var length = form.length.value;
+	var statement = "add&name="+name+"&category="+category+"&length="+length;
+	emptyElements('videos');
+	request(statement);
+}
+
+function deleteFunction(elem) {
+	console.log(elem.id);
+	var statement = "delete&id=" + elem.id;
+	emptyElements('videos');
+	request(statement);
+}
+
+function flipRent(elem) {
+	console.log(elem.id);
+	var statement = 'rent&id=' + elem.id;
+	emptyElements('videos');
+	request(statement);
+}
+
+function emptyElements(elem) {
+	var container = document.getElementById(elem);
+	var elements = document.getElementsByClassName('video');
+	var id;
+	while (elements.length) {
+		id = elements[0].id;
+		container.removeChild(document.getElementById(id));
+	}
+}
--- videostore.php
+++ videostore.php
@@ -1,14 +1,60 @@
 <?php 
+	$table = 'TEST';
 	$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "iannie-db", "6kRBxp0O6k2hf4rb", "iannie-db");
 	if ($mysqli->connect_errno) {
     	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	}
 
-	$results = $mysqli->query("SELECT * FROM TEST");
-	$list = array();
-	while($row = $results->fetch_assoc())
-	{
-		array_push($list, $row);
+	function add($table, $mysqli) {
+		$name = $_GET['name'];
+		$length = $_GET['length'];
+		$category = $_GET['category'];
+		$rented = 0;
+		$insert = $mysqli->prepare("INSERT INTO $table(name, category, length, rented) VALUES (?, ?, ?, ?)");
+		$insert->bind_param('ssii', $name, $category, $length, $rented);
+		$insert->execute();
+		show($table, $mysqli);
 	}
-	echo json_encode($list);
+
+	function del($table, $mysqli) {
+		$insert = $mysqli->prepare("DELETE FROM $table WHERE id = ?");
+		$insert->bind_param('i', $_GET['id']);
+		$insert->execute();
+		show($table, $mysqli);
+	}
+
+	function rent($table, $mysqli) {
+		$insert = $mysqli->prepare("UPDATE $table SET rented = ? WHERE id = ?");
+		$insert->bind_param('ii', 1, $_GET['id']);
+		$insert->execute();
+		show($table, $mysqli);
+	}
+
+	function show($table, $mysqli) {
+		$results = $mysqli->query("SELECT * FROM $table");
+		$list = array();
+		while($row = $results->fetch_assoc())
+		{
+			array_push($list, $row);
+		}
+		echo json_encode($list);
+	}
+
+	if($_GET['action'] === 'load') {
+		show($table, $mysqli);
+	} else {
+		if($_GET['action'] === 'add') {
+			add($table, $mysqli);
+		} else {
+			if($_GET['action'] === 'delete') {
+				del($table, $mysqli);
+			} else {
+				if($_GET['action'] === 'rent') {
+					rent($table, $mysqli);
+				}
+			}
+		}
+	}
+	
+
  ?>
